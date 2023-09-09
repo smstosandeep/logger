c54ec5f528bbba81c9fdc71692aa255340a65560
Sandy <smstosandeep@gmail.com>
2023-09-09 17:12:02 +0530

    Delete dist directory

--- dist/component/logger.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * Sandeep Sirohi: Logger.
- */
-export declare class Logger {
-    static info(msg: string): void;
-    static error(msg: string): void;
-    static warn(msg: string): void;
-}
